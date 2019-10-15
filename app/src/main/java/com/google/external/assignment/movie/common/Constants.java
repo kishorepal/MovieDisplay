59789702ddd6ee2848d8ead8d8ee2801f8bbd8f7
kishorepal <Kishore.Pal86@gmail.com>
2019-10-15 22:34:05 +0900

    Stage 1 Source Code Final

--- app/src/main/java/com/google/external/assignment/movie/common/Constants.java
+++ app/src/main/java/com/google/external/assignment/movie/common/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String SORT_BY_TOP_RATED = "top_rated";
 
     // FIXME Change the API Key
-    public static final String API_KEY_MOVIE_DB = BuildConfig.MOVIEDB_API_KEY;//"da61481c696b839e4db3c86449141754";
+    public static final String API_KEY_MOVIE_DB = BuildConfig.MOVIEDB_API_KEY;
     public static final String BASE_URL_MOVIE_DB = "https://api.themoviedb.org/3/";
     public static final String BASE_URL_PICASSO = "http://image.tmdb.org/t/p/w780%s";
 }
